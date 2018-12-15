076bf012cfe851779a4dd82c1e3b506ae528199e
Maliarets <MMaliarets@DBDC.luxoft.com>
2018-12-15 12:15:13 +0100

    initial commit

---
+++ src/components/Accounts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Accounts extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO Accounts</h2>
+
+      </div>
+    );
+  }
+}
+
+export default Accounts;
---
+++ src/components/Budget.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Budget extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO Budget</h2>
+
+      </div>
+    );
+  }
+}
+
+export default Budget;
